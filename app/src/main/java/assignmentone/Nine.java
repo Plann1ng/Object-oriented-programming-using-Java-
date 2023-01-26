9116e6e7e0d9e0661f54e2c13dd2010b221f000a
Ryustem Shaban <rstmduran@gmail.com>
2023-01-26 23:40:35 +0100

    User win case added

--- app/src/main/java/assignmentone/Nine.java
+++ app/src/main/java/assignmentone/Nine.java
@@ -96,3 +96,9 @@ public class Nine {
                       System.out.print("Computer rolls again and gets " + comp2);
                       System.out.print(", Computer got " + computer + " in total.\n");
                       System.out.println("\nYou lost!\n");
+                    } else if (finalvalue > computer) {
+
+                        System.out.println("Computer rolled " + comp);
+                        System.out.print("Computer rolls again and gets " + comp2);
+                        System.out.print(", Computer got " + computer + " in total.");
+                        System.out.println("\nYou won!\n");
