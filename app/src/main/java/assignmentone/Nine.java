5d35768064da0e5ff1fb01670f7c79d60ff4accb
Ryustem Shaban <rstmduran@gmail.com>
2023-01-26 23:33:43 +0100

    Simple dice game created

---
+++ app/src/main/java/assignmentone/Nine.java
@@ -0,0 +1,19 @@
+package assignmentone;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/** Dice program.
+ */
+public class Nine {
+
+  /**
+   * The dice game simulator,
+   * may the force be with you or you will be FAT!.
+   */
+  public static void main(String[] args) {
+
+    // User input to start the program
+    Scanner input = new Scanner(System.in, "UTF-8");
+    System.out.println("Wanna play a game? (Y / N) ?\n");
+    String variableplay = input.nextLine();
