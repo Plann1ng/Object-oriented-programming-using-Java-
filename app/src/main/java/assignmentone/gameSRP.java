c7b2b4485195b4f55eff199b57fa0fe6c5c104bc
Ryustem Shaban <rstmduran@gmail.com>
2023-01-26 23:25:50 +0100

    Initial logic implementation minor commit

--- app/src/main/java/assignmentone/gameSRP.java
+++ app/src/main/java/assignmentone/gameSRP.java
@@ -20,3 +20,14 @@ public class Gamesrp {
 
     Random comp = new Random();
     Scanner choice = new Scanner(System.in, "UTF-8"); 
+    while (true) {
+        //Choosing random between 1-3 for the computer's selection
+        int computer = comp.nextInt(1, 4);
+  
+        System.out.println("Scissor (1), rock (2), paper (3) or 0 to quit: ");
+  
+        int finalchoice = Integer.parseInt(choice.nextLine());
+  
+        /* Checks all possibilities when user choise is 1 and increments
+         * the score correspondingly
+         */
