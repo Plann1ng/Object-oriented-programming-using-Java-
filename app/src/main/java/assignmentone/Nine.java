8fe4a6690b6720fd417e59dddd74d2518b5a34a9
Ryustem Shaban <rstmduran@gmail.com>
2023-01-26 23:52:02 +0100

    Indentation fixed

--- app/src/main/java/assignmentone/Nine.java
+++ app/src/main/java/assignmentone/Nine.java
@@ -19,101 +19,101 @@ public class Nine {
     String variableplay = input.nextLine();
 
     if (variableplay.equals("N")) {
-        System.out.println("See you next time!.");
-  
-        // Invalid input interpretation
-      } else if (!variableplay.equals("N") && !variableplay.equals("Y")) {
-        System.out.println("Please follow the instructions.");
-  
-        // Game starts
-      } else if (variableplay.equals("Y")) {
-          Random dice = new Random();
-          int player = dice.nextInt(1, 7);
-          int comp = dice.nextInt(1, 7);
-          System.out.println("\nYou rolled: " + player);
-          System.out.println("\nYou want to roll once more?\n");
-          String userchoice = input.nextLine();
-          // Second roll
-        if (!userchoice.equals("Y") && !userchoice.equals("N")) {
+      System.out.println("See you next time!.");
 
-             System.out.println("Please follow the instructions!");
+      // Invalid input interpretation
+    } else if (!variableplay.equals("N") && !variableplay.equals("Y")) {
+      System.out.println("Please follow the instructions.");
 
-        } else if (userchoice.equals("Y")) {
+      // Game starts
+    } else if (variableplay.equals("Y")) {
+      Random dice = new Random();
+      int player = dice.nextInt(1, 7);
+      int comp = dice.nextInt(1, 7);
+      System.out.println("\nYou rolled: " + player);
+      System.out.println("\nYou want to roll once more?\n");
+      String userchoice = input.nextLine();
+      // Second roll
+      if (!userchoice.equals("Y") && !userchoice.equals("N")) {
 
-          // Cheking if the computer rolled 4 or more, if it is the case the computer
-          // won't do another roll
+        System.out.println("Please follow the instructions!");
+
+      } else if (userchoice.equals("Y")) {
+
+        // Cheking if the computer rolled 4 or more, if it is the case the computer
+        // won't do another roll
         if (comp >= 4) {
           int playerchois = dice.nextInt(1, 7);
           int fnlvalue = playerchois + player;
           System.out.print("\nYour rolled " + playerchois);
           System.out.print(", in total you have " + fnlvalue + "\n");
 
-              // Instance where after the second roll user gets number higher than 9
-              if (fnlvalue > 9) {
+          // Instance where after the second roll user gets number higher than 9
+          if (fnlvalue > 9) {
 
-                System.out.println("\nfat\n");
-                // When the user does not cross the 9 limitation
-              } else {
-    
-                // User loses.
-                if (fnlvalue < comp) {
-                  System.out.println("\nComputer rolled " + comp);
-                  System.out.println("\nYou lost!\n");
+            System.out.println("\nfat\n");
+            // When the user does not cross the 9 limitation
+          } else {
 
-            // User wins
+            // User loses.
+            if (fnlvalue < comp) {
+              System.out.println("\nComputer rolled " + comp);
+              System.out.println("\nYou lost!\n");
+
+              // User wins
             } else if (fnlvalue > comp) {
 
-                System.out.println("Computer rolled " + comp);
-                System.out.println("\nYou won!\n");
-                // Draw
-              } else if (comp == fnlvalue) {
-  
-                System.out.println("Computer rolled " + comp);
-                System.out.println("\nIt is a draw!\n");
-  
-              }
+              System.out.println("Computer rolled " + comp);
+              System.out.println("\nYou won!\n");
+              // Draw
+            } else if (comp == fnlvalue) {
+
+              System.out.println("Computer rolled " + comp);
+              System.out.println("\nIt is a draw!\n");
+
             }
+          }
           // Instance when the computer does rolls less than 4 on the first rolled dice
         } else if (comp <= 4) {
 
-            int playerchoice = dice.nextInt(1, 7);
-            int finalvalue = playerchoice + player;
-            int comp2 = dice.nextInt(1, 7);
-            int computer = comp + comp2;
-            System.out.print("\nYour rolled " + playerchoice);
-            System.out.print(", in total you have " + finalvalue + "\n");
-            // User rolls more than 9
-            if (finalvalue > 9) {
-  
-              System.out.println("\nfat!\n");
+          int playerchoice = dice.nextInt(1, 7);
+          int finalvalue = playerchoice + player;
+          int comp2 = dice.nextInt(1, 7);
+          int computer = comp + comp2;
+          System.out.print("\nYour rolled " + playerchoice);
+          System.out.print(", in total you have " + finalvalue + "\n");
+          // User rolls more than 9
+          if (finalvalue > 9) {
 
-                    // Validating that both participants are below 9
-                } else if (finalvalue <= 9 && computer <= 9) {
-                    // User loses
-                    if (finalvalue < computer) {
-        
-                      System.out.println("\nComputer rolled " + comp);
-                      System.out.print("Computer rolls again and gets " + comp2);
-                      System.out.print(", Computer got " + computer + " in total.\n");
-                      System.out.println("\nYou lost!\n");
-                    // User wins
-                    } else if (finalvalue > computer) {
+            System.out.println("\nfat!\n");
 
-                        System.out.println("Computer rolled " + comp);
-                        System.out.print("Computer rolls again and gets " + comp2);
-                        System.out.print(", Computer got " + computer + " in total.");
-                        System.out.println("\nYou won!\n");
+            // Validating that both participants are below 9
+          } else if (finalvalue <= 9 && computer <= 9) {
+            // User loses
+            if (finalvalue < computer) {
 
-                              // Draw
+              System.out.println("\nComputer rolled " + comp);
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.\n");
+              System.out.println("\nYou lost!\n");
+              // User wins
+            } else if (finalvalue > computer) {
+
+              System.out.println("Computer rolled " + comp);
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.");
+              System.out.println("\nYou won!\n");
+
+              // Draw
             } else if (computer == finalvalue) {
 
-                System.out.println("Computer rolled " + comp);
-                System.out.print("Computer rolls again and gets " + comp2);
-                System.out.print(", Computer got " + computer + " in total.");
-                System.out.println("\nIt is a draw!y\n");
-              }
+              System.out.println("Computer rolled " + comp);
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.");
+              System.out.println("\nIt is a draw!y\n");
+            }
             // If the computer is above 9
-        } else {
+          } else {
 
             System.out.println("Computer rolled " + comp);
             System.out.println("Computer rolls again and gets " + comp2);
@@ -123,12 +123,12 @@ public class Nine {
           }
         }
         // If user does not starts the game
-    } else if (variableplay.equals("N")) {
+      } else if (variableplay.equals("N")) {
 
         System.out.println("\nSee you next times!\n");
 
         // No for second roll
-    } else if (userchoice.equals("N")) {
+      } else if (userchoice.equals("N")) {
         // Computer rolls 1 time
         if (comp >= 4) {
 
@@ -151,49 +151,49 @@ public class Nine {
             System.out.println("\nIt is a draw!\n");
 
           }
-                    // Computer rolls twice
+          // Computer rolls twice
         } else if (comp <= 4) {
 
-            int comp2 = dice.nextInt(1, 3);
-            int computer = comp + comp2;
-            System.out.print("\nYour rolled " + player);
-            System.out.print(", in total you have " + player + "\n");
-            // Validation for limitations
-            if (player <= 9 && computer <= 9) {
-              // User loses
-              if (player < computer) {
-  
-                System.out.println("\nComputer rolled " + comp);
-                System.out.print("Computer rolls again and gets " + comp2);
-                System.out.print(", Computer got " + computer + " in total.\n");
-                System.out.println("\nYou lost!\n");
-                          // User wins
-                        } else if (player > computer) {
+          int comp2 = dice.nextInt(1, 3);
+          int computer = comp + comp2;
+          System.out.print("\nYour rolled " + player);
+          System.out.print(", in total you have " + player + "\n");
+          // Validation for limitations
+          if (player <= 9 && computer <= 9) {
+            // User loses
+            if (player < computer) {
 
-                            System.out.println("Computer rolled " + comp);
-                            System.out.print("Computer rolls again and gets " + comp2);
-                            System.out.print(", Computer got " + computer + " in total.");
-                            System.out.println("\nYou won!\n");
-                                          // Draw
+              System.out.println("\nComputer rolled " + comp);
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.\n");
+              System.out.println("\nYou lost!\n");
+              // User wins
+            } else if (player > computer) {
+
+              System.out.println("Computer rolled " + comp);
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.");
+              System.out.println("\nYou won!\n");
+              // Draw
             } else if (computer == player) {
 
-                System.out.println("Computer rolled " + comp);
-                System.out.print("Computer rolls again and gets " + comp2);
-                System.out.print(", Computer got " + computer + " in total.");
-                System.out.println("\nIt is a draw!y\n");
-  
-                input.close();
-              }
-              // Computer above 9
-            } else if (computer > 9) {
-  
               System.out.println("Computer rolled " + comp);
-              System.out.println("Computer rolls again and gets " + comp2);
-              System.out.print("Computer got " + computer + " in total.");
-              System.out.println("\nYou won!\n");
+              System.out.print("Computer rolls again and gets " + comp2);
+              System.out.print(", Computer got " + computer + " in total.");
+              System.out.println("\nIt is a draw!y\n");
+
+              input.close();
             }
+            // Computer above 9
+          } else if (computer > 9) {
+
+            System.out.println("Computer rolled " + comp);
+            System.out.println("Computer rolls again and gets " + comp2);
+            System.out.print("Computer got " + computer + " in total.");
+            System.out.println("\nYou won!\n");
           }
         }
       }
     }
   }
+}
