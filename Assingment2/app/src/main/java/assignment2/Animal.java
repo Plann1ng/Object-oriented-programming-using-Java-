ce0085b7a4970b2380e1d21d703d737b951da843
Ryustem Shaban <rstmduran@gmail.com>
2023-01-28 23:41:26 +0100

    Animal object paramaters passed

--- Assingment2/app/src/main/java/assignment2/Animal.java
+++ Assingment2/app/src/main/java/assignment2/Animal.java
@@ -10,3 +10,33 @@ public abstract class Animal {
   protected String sound;
 
   public Animal() {}
+
+    /**
+   * Animal object with getters and setters and compareTo method which is
+   * unique for abstract classes.
+   */
+  public Animal(String name, String latinName, double weight, String sound) {
+    if (name.isEmpty()) {
+      throw new IllegalArgumentException("Name can not be empty!");
+    } else {
+      this.name = name;
+    }
+
+    if (latinName.isEmpty()) {
+      throw new IllegalArgumentException("Latin Name can not be empty!");
+    } else {
+      this.latinName = latinName;
+    }
+
+    if (weight <= 0) {
+      throw new IllegalArgumentException("Weight can not be 0!");
+    } else {
+      this.weight = weight;
+    }
+
+    if (sound.isEmpty()) {
+      throw new IllegalArgumentException("Sound can not be empty!");
+    } else {
+      this.sound = sound;
+    }
+  }
