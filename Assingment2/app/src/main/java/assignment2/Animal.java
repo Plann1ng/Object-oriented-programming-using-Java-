069d956853c12d4f24a4296a3e10fac9167775d4
Ryustem Shaban <rstmduran@gmail.com>
2023-01-28 23:40:45 +0100

    Started with assignment 2 tasks

---
+++ Assingment2/app/src/main/java/assignment2/Animal.java
@@ -0,0 +1,12 @@
+package assignment2.animal;
+
+/**
+ * Abstract Animal class containing common constructors for all instances of Animals.
+ */
+public abstract class Animal {
+  private String name;
+  private String latinName;
+  private double weight;
+  protected String sound;
+
+  public Animal() {}
