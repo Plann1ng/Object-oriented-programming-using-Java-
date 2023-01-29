063e9bd1f0c54d12d03573afb2c4b3f0951c8850
Ryustem Shaban <rstmduran@gmail.com>
2023-01-29 21:10:51 +0100

    Mammal object added

---
+++ Assingment2/app/src/main/java/assignment2/animal/Mammal.java
@@ -0,0 +1,25 @@
+package assignment2.animal;
+
+/**
+ * Mammal class extending abstract class Animal.
+ */
+public class Mammal extends Animal {
+
+  private String furColor;
+  private boolean hasWinterfur;
+
+  public Mammal() {}
+
+  /**
+   * Mammal object with stated methods required from the assignment.
+   */
+  public Mammal(String name, String latinName, double weight,
+      String sound, String furColor, boolean hasWinterfur) {
+    super(name, latinName, weight, sound);
+    if (furColor.isEmpty()) {
+      throw new IllegalArgumentException("FurColor can not be empty!");
+    } else {
+      this.furColor = furColor;
+    }
+    this.hasWinterfur = hasWinterfur;
+  }
