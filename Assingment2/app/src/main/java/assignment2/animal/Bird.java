5033edd7b7ebc4ba8b381407381ea9ed77402f4d
Ryustem Shaban <rstmduran@gmail.com>
2023-01-29 21:08:35 +0100

    Started on the bird object

---
+++ Assingment2/app/src/main/java/assignment2/animal/Bird.java
@@ -0,0 +1,25 @@
+package assignment2.animal;
+
+/**
+ * Bird class extending Animal.
+ */
+public class Bird extends Animal {
+  private boolean isMigrant;
+  private boolean canFly;
+  private String nestType;
+
+  /**
+   * Bird object with unique constructors for Birds and super constructors from the abstract Animal.
+   */
+  public Bird(String name, String latinName, double weight, 
+      String sound, boolean isMigrant, boolean canFly,
+      String nestType) {
+    super(name, latinName, weight, sound);
+    this.isMigrant = isMigrant;
+    this.canFly = canFly;
+    if (nestType.isEmpty()) {
+      throw new IllegalArgumentException("Nest type can not be empty.");
+    } else {
+      this.nestType = nestType;
+    }
+  }
