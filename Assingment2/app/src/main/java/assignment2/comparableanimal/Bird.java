e7d200a95b77930b6d11856555fdcfb3f1609efc
Ryustem Shaban <rstmduran@gmail.com>
2023-01-29 21:16:55 +0100

    comparable bird class added

---
+++ Assingment2/app/src/main/java/assignment2/comparableanimal/Bird.java
@@ -0,0 +1,62 @@
+package assignment2.comparableanimal;
+
+/**
+ * Bird class extending Animal.
+ */
+public class Bird extends Animal {
+  private boolean isMigrant;
+  private boolean canFly;
+  private String nestType;
+
+  /**
+   * Bird object with unique constructors for Birds and super constructors from the abstract Animal.
+   */
+  public Bird(String name, String latinName, double weight, 
+      String sound, boolean isMigrant, boolean canFly,
+      String nestType) {
+    super(name, latinName, weight, sound);
+    this.isMigrant = isMigrant;
+    this.canFly = canFly;
+    if (nestType.isEmpty()) {
+      throw new IllegalArgumentException("Nest type can not be empty.");
+    } else {
+      this.nestType = nestType;
+    }
+  }
+
+  public boolean isMigrant() {
+    return isMigrant;
+  }
+
+  public void setMigrant(boolean isMigrant) {
+    this.isMigrant = isMigrant;
+  }
+
+  public boolean isCanFly() {
+    return canFly;
+  }
+
+  public void setCanFly(boolean canFly) {
+    this.canFly = canFly;
+  }
+
+  public String getNestType() {
+    return nestType;
+  }
+
+  @Override
+  public String makeSound() {
+    return " tweets: " + sound;
+  }
+
+  /**
+   * Setter for the nest type.
+   */
+  public void setNestType(String nestType) {
+    if (nestType.isEmpty()) {
+      throw new IllegalArgumentException("Nest type can not be empty.");
+    } else {
+      this.nestType = nestType;
+    }
+  }
+}
