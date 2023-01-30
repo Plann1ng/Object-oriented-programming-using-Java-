88b61398e1acc8f6235e86f23c1b746f215f0ac6
Ryustem Shaban <rstmduran@gmail.com>
2023-01-30 22:38:43 +0100

    getters and setters added and finalized class

--- Assingment2/app/src/main/java/assignment2/planet/Planet.java
+++ Assingment2/app/src/main/java/assignment2/planet/Planet.java
@@ -48,3 +48,87 @@ public class Planet {
         this.perihelion = perihelion;
       }
     }
+
+    public String getName() {
+        return name;
+      }
+    
+      /**
+       * Flexibility related method to change Planet object's name under conditions.
+       */
+      public void setName(String name) {
+        if (name.isEmpty()) {
+          throw new IllegalArgumentException("Name can not be empty!");
+        } else {
+          this.name = name;
+        }
+      }
+    
+      public Integer getPosition() {
+        return position;
+      }
+    
+      /**
+       * Flexibility focused position method for Planet object.
+       */
+      public void setPosition(Integer position) {
+        if (position <= 0 || position >= 8) {
+          throw new IllegalArgumentException("Position must be positive integer between 0 and 8");
+        } else {
+          this.position = position;
+        }
+      }
+    
+      /**
+       * Basic method to return number of moons added to the ArrayList.
+       */
+      public Integer getNoOfMoons() {
+        return noOfMoons;
+      }
+    
+      /**
+       * Moon setter method with restrictions.
+       */
+      public void setNoOfMoons(Integer moonNumber) {
+        if (moonNumber < 0) {
+          throw new IllegalArgumentException("Moon Number must be positive integer!");
+        } else {
+          this.noOfMoons = moonNumber;
+        }
+      }
+    
+      /**
+       * Returning Aphelion.
+       */
+      public Integer getAphelion() {
+        return aphelion;
+      }
+    
+      /**
+       * setAphelion Method which gives possiblity to change the Aphelion in
+       * case that the Aphelion size in Real World becomes bigger or smaller.
+       */
+      public void setAphelion(Integer aphelion) {
+        if (aphelion <= 0) {
+          throw new IllegalArgumentException("Aphelion must be positive integer!");
+        } else {
+          this.aphelion = aphelion;
+        }
+      }
+    
+      public Integer getPerihelion() {
+        return perihelion;
+      }
+    
+      /**
+       * setPerihelion method provides flexibility to adjust Planet perihelion.
+       */
+      public void setPerihelion(Integer perihelion) {
+        if (perihelion <= 0) {
+          throw new IllegalArgumentException("Perihelion must be positive integer!");
+        } else {
+          this.perihelion = perihelion;
+        }
+      }
+    }
+    
