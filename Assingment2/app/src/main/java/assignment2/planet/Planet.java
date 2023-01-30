e767d9a15b84d45610af66c7887f80c679126f3d
Ryustem Shaban <rstmduran@gmail.com>
2023-01-30 22:35:52 +0100

    Began implementation of the planet class

---
+++ Assingment2/app/src/main/java/assignment2/planet/Planet.java
@@ -0,0 +1,14 @@
+package assignment2.planet;
+
+/**
+ * Planet object which contains Moon objects within.
+ */
+public class Planet {
+  private String name;
+  private Integer position;
+  private Integer aphelion;
+  private Integer perihelion;
+  private Integer noOfMoons;
+
+  public Planet() {
+  }
