60f00e632a92240d1c7b9e727112fd3738f798f7
Ryustem Shaban <rstmduran@gmail.com>
2023-01-30 22:45:00 +0100

    Constructtors added

--- Assingment2/app/src/main/java/assignment2/planetwithmoons/Planet.java
+++ Assingment2/app/src/main/java/assignment2/planetwithmoons/Planet.java
@@ -15,3 +15,39 @@ public class Planet {
 
   public Planet() {
   }
+
+    /**
+   * Planet object.
+   */
+  public Planet(String name, Integer position, Integer noOfMoons, Integer aphelion,
+      Integer perihelion) {
+    if (name.isEmpty()) {
+      throw new IllegalArgumentException("Name can not be empty!");
+    } else {
+      this.name = name;
+    }
+
+    if (position <= 0 || position >= 8) {
+      throw new IllegalArgumentException("Position must be positive integer between 0 and 8");
+    } else {
+      this.position = position;
+    }
+
+    if (aphelion <= 0) {
+      throw new IllegalArgumentException("Aphelion must be positive integer!");
+    } else {
+      this.aphelion = aphelion;
+    }
+
+    if (noOfMoons < 0) {
+      throw new IllegalArgumentException("Moons can not be negative number!");
+    } else {
+      this.noOfMoons = noOfMoons;
+    }
+
+    if (perihelion <= 0) {
+      throw new IllegalArgumentException("Perihelion must be positive integer!");
+    } else {
+      this.perihelion = perihelion;
+    }
+  }
